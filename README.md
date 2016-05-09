43475eccc942698d48c648eb6546b3bf2ee3e7e6
Jelle Sjollema <j.sjollema@ma-web.nl>
2016-05-09 12:25:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# pickACard
+
+* opdracht pickACard
+** maak een applicatie die uit een willekeurige stapel kaarten
+** na een click op een omgekeerde kaart
+** een willekeurige kaart uit de stapel selecteert
+** zie http://mathportfolio.org/Choose_a_playingcard
---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("load",function(){
+    //opdracht pickACard
+    //maak een applicatie die uit een willekeurige stapel kaarten
+    //na een click op een omgekeerde kaart
+    //een willekeurige kaart uit de stapel selecteert
+    //zie http://mathportfolio.org/Choose_a_playingcard
+
+
+	var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var sx,sy,sw,sh;
+    var counter = 0;
+
+    var image = new Image();
+    image.src = "media/images/cardDeck.png";
+
+})
